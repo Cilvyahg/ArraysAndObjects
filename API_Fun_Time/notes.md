58fad95f6e8b311f7333354a2812251b9f2cfaad
Cilvya <cilvya.hg@gmail.com>
2022-09-29 17:54:13 +0200

    link added

---
+++ API_Fun_Time/notes.md
@@ -0,0 +1,7 @@
+
+### pseudo-elements
+pseudo elements don't work on images
+they arre inline by default so you can use inline-block
+
+::before, ::after
+
--- API_Fun_Time/script.js
+++ API_Fun_Time/script.js
@@ -2,27 +2,35 @@
 
 const log = console.log;
 
+const btnBored = document.getElementById('btn-bored');
+const activitiesList = document.querySelector('.activitiesToDo');
+
 const fetchingBoredAPI = async function () {
   try {
     const response = await fetch('http://www.boredapi.com/api/activity/', {
       method: 'GET',
     });
-    const json = await response.json();
-    log(json)
-    const activityArray = Object.values(json)
-    log(activityArray)
-   
+    const activitiesData = await response.json();
+    log(activitiesData);
+
+    const li1 = activitiesList.children[0];
+    const li2 = activitiesList.children[1];
+    const li3 = activitiesList.children[2];
+    const li4 = activitiesList.children[3];
+
+    log(li1, li2, li3, li4);
+
+    li1.innerHTML = `<strong>Activity</strong> : ${activitiesData.activity}`
+    li2.innerHTML = `<strong>Type</strong> : ${activitiesData.activity}`;
+    li3.innerHTML = `<strong>Participants</strong> : ${activitiesData.activity}`;
+    li4.innerHTML = `<strong>Link</strong> : <a href= ${activitiesData.link}>${activitiesData.link}</a>`;
 
 
   } catch (err) {
     log(`this is the error: ${err}`);
   }
-}
-
-
-
-const btnBored = document.getElementById('btn-bored');
+};
 
 btnBored.addEventListener('click', function () {
   fetchingBoredAPI();
-})
+});
