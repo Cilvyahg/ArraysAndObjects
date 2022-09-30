a08c2aa05f100c2fca1733f234e1c6c4542494d8
Cilvya <cilvya.hg@gmail.com>
2022-09-30 16:03:24 +0200

    padding

--- API_Fun_Time/script.js
+++ API_Fun_Time/script.js
@@ -5,6 +5,7 @@ const log = console.log;
 const btnBored = document.getElementById('btn-bored');
 const activitiesList = document.querySelector('.activitiesToDo');
 const container = activitiesList.parentElement;
+const intro = document.getElementById('intro-text');
 
 const fetchingBoredAPI = async function () {
   try {
@@ -13,37 +14,41 @@ const fetchingBoredAPI = async function () {
     });
     const activitiesData = await response.json();
 
-    const allLi = document.querySelectorAll('.li')
+    intro.style.display = 'none';
+    const allLi = document.querySelectorAll('.li');
 
-    const li1 = allLi[0]
-    const li2 = allLi[1]
-    const li3 = allLi[2]
-    const li4 = allLi[3]
-    
+    const li1 = allLi[0];
+    const li2 = allLi[1];
+    const li3 = allLi[2];
+    const li4 = allLi[3];
+
     li1.innerHTML = `<strong>Activity</strong>: ${activitiesData.activity}`;
-    li2.innerHTML = `<strong>Type</strong>: ${activitiesData.activity}`;
-    li3.innerHTML = `<strong>Participants</strong>: ${activitiesData.activity}`;
-    li4.innerHTML = `<strong>Link</strong>: <a href= ${activitiesData.link}>${activitiesData.link}</a>`;
-  
+    li2.innerHTML = `<strong>Type</strong>: ${activitiesData.type}`;
+    li3.innerHTML = `<strong>Participants</strong>: ${activitiesData.participants}`;
+    log(Boolean(activitiesData.link));
+
+    if (activitiesData.link) {
+      li4.innerHTML = `<strong>Link</strong>: <a href= ${activitiesData.link}>${activitiesData.link}</a>`;
+    } else {
+      li4.innerHTML = '';
+    }
   } catch (error) {
     log(`THIS IS THE ERROR ${error}`);
   }
 };
 
-
 btnBored.addEventListener('click', function () {
   fetchingBoredAPI();
 });
 
-const activitiesStyling = async () => {
- container.style.visibility = 'visible';
- container.style.opacity = '1';
-}
-
+const activitiesStyling = () => {
+  container.style.opacity = '1';
+  // container.style.opacity = '0';
+};
 
 btnBored.addEventListener('click', function (e) {
-  activitiesStyling()
-})
+  activitiesStyling();
+});
 
 // TODO ALS LINK LEEG IS LAAT HET DAN NIET ZIEN
 // HET VENSTER MOET IN HET BEGIN LEEG ZIJN.
@@ -51,3 +56,14 @@ btnBored.addEventListener('click', function (e) {
 // desktopversie
 // min width max width
 // position absolute;
+
+document.getElementById('hoi').addEventListener('click', function (e) {
+  log('clicked');
+
+  this.style.transition = 'opacity 0s';
+  this.style.opacity = 0;
+  setTimeout(() => {
+    this.style.transition = 'opacity 1s linear';
+    this.style.opacity = 1;
+  }, 3000);
+});
