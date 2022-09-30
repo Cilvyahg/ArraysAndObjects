f59485dd4f72ece493362d7b769a8cdfb853e1e2
Cilvya <cilvya.hg@gmail.com>
2022-09-30 19:44:45 +0200

    flip

--- API_Fun_Time/script.js
+++ API_Fun_Time/script.js
@@ -6,6 +6,7 @@ const btnBored = document.getElementById('btn-bored');
 const activitiesList = document.querySelector('.activitiesToDo');
 const container = activitiesList.parentElement;
 const intro = document.getElementById('intro-text');
+const allLi = document.querySelectorAll('.li');
 
 const fetchingBoredAPI = async function () {
   try {
@@ -15,7 +16,6 @@ const fetchingBoredAPI = async function () {
     const activitiesData = await response.json();
 
     intro.style.display = 'none';
-    const allLi = document.querySelectorAll('.li');
 
     const li1 = allLi[0];
     const li2 = allLi[1];
@@ -25,45 +25,41 @@ const fetchingBoredAPI = async function () {
     li1.innerHTML = `<strong>Activity</strong>: ${activitiesData.activity}`;
     li2.innerHTML = `<strong>Type</strong>: ${activitiesData.type}`;
     li3.innerHTML = `<strong>Participants</strong>: ${activitiesData.participants}`;
+    log(activitiesData);
     log(Boolean(activitiesData.link));
 
     if (activitiesData.link) {
-      li4.innerHTML = `<strong>Link</strong>: <a href= ${activitiesData.link}>${activitiesData.link}</a>`;
+      li4.innerHTML = `<a href= ${activitiesData.link} target="_blank">Visit Page</a>`;
+      // als het true is dat er een link value is dan maak dit aan
     } else {
-      li4.innerHTML = '';
+      li4.innerHTML = ''; // je moet wel een false zetten en de lijst leegmaken, anders blijft het de volgende keer gewoon de oude link staan omdat je niet zegt wat je moet doen met false
     }
   } catch (error) {
     log(`THIS IS THE ERROR ${error}`);
   }
 };
 
-btnBored.addEventListener('click', function () {
-  fetchingBoredAPI();
-});
 
 const activitiesStyling = () => {
-  container.style.opacity = '1';
-  // container.style.opacity = '0';
+  container.style.opacity = 0;
+  allLi.forEach(function (li) {
+    li.style.opacity = 0;
+  });
+  
+  setTimeout(() => {
+    container.style.transition = 'opacity 100ms ease-out';
+    container.style.opacity = 1;
+    
+    for (let li of allLi) {
+      li.style.transition = 'opacity 0ms 50ms';
+      li.style.opacity = 1;
+    }
+  }, 300);
+  
+  
 };
 
-btnBored.addEventListener('click', function (e) {
-  activitiesStyling();
-});
 
-// TODO ALS LINK LEEG IS LAAT HET DAN NIET ZIEN
-// HET VENSTER MOET IN HET BEGIN LEEG ZIJN.
-// andere functies schrijven cleaner
-// desktopversie
-// min width max width
-// position absolute;
+btnBored.addEventListener('click', fetchingBoredAPI);
+btnBored.addEventListener('click', activitiesStyling);
 
-document.getElementById('hoi').addEventListener('click', function (e) {
-  log('clicked');
-
-  this.style.transition = 'opacity 0s';
-  this.style.opacity = 0;
-  setTimeout(() => {
-    this.style.transition = 'opacity 1s linear';
-    this.style.opacity = 1;
-  }, 3000);
-});
