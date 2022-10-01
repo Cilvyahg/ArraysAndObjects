c3f19e0ca4c746c8c19ca4192c85f7ed6dfc40c6
Cilvya <cilvya.hg@gmail.com>
2022-10-01 12:00:49 +0200

    media

--- API_Fun_Time/script.js
+++ API_Fun_Time/script.js
@@ -10,10 +10,10 @@ const allLi = document.querySelectorAll('.li');
 
 const fetchingBoredAPI = async function () {
   try {
-    const response = await fetch('http://www.boredapi.com/api/activity/', {
+    const response = await fetch('https://www.boredapi.com/api/activity/', {
       method: 'GET',
     });
-    
+
     const activitiesData = await response.json();
 
     intro.style.display = 'none';
@@ -35,32 +35,45 @@ const fetchingBoredAPI = async function () {
     } else {
       li4.innerHTML = ''; // je moet wel een false zetten en de lijst leegmaken, anders blijft het de volgende keer gewoon de oude link staan omdat je niet zegt wat je moet doen met false
     }
-  } catch (error) { // error is a object
+  } catch (error) {
+    // error is a object
     return `THIS IS THE ERROR ${error}`;
   }
 };
-
 
 const activitiesStyling = () => {
   container.style.opacity = 0;
   allLi.forEach(function (li) {
     li.style.opacity = 0;
   });
-  
+
   setTimeout(() => {
     container.style.transition = 'opacity 100ms ease-out';
     container.style.opacity = 1;
-    
+
     for (let li of allLi) {
       li.style.transition = 'opacity 0ms 50ms';
       li.style.opacity = 1;
     }
   }, 300);
-  
-  
 };
-
 
 btnBored.addEventListener('click', fetchingBoredAPI);
 btnBored.addEventListener('click', activitiesStyling);
 
+// async function af() {
+//   try {
+//     throw new Error('hi');
+//   } catch (e) {
+//     return 0;
+//   }
+// }
+
+// // a().then(a => log(a)).catch(a => log(a));
+
+// async function b() {
+//   let a = await af();
+//   log(a)
+// }
+
+// b();
